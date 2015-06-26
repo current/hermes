a742fc394ceccf7a2888d680e8a3fde6e750a3aa
Daniel Libanori <daniellibanori@gmail.com>
2015-06-26 04:43:28 -0300

    I18n business

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,7 @@
 module UsersHelper
+  def business_choices
+    User::BUSINESS.map do |business|
+      [ t(".#{business}"), business ]
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ActiveRecord::Base
+  BUSINESS = %w[ medical dental aesthetic ]
   has_many :appointments
 
   has_secure_password
 
+  validates_inclusion_of :business, in: BUSINESS
   validates_uniqueness_of :email, case_sensitive: false
   validates_presence_of :email, :name
 end
