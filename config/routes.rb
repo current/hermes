a1abbfe2da304b52d7db7411d802a1b3d041b22b
Daniel Libanori <daniellibanori@gmail.com>
2015-06-26 05:39:50 -0300

    Add settings routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   get 'logout' => 'sessions#destroy'
   get 'signup' => 'users#new'
   post 'signup' => 'users#create'
+  get 'settings' => 'users#edit'
+  patch 'settings' => 'users#update'
 
   resources :appointments do
     get :status, :on => :member
