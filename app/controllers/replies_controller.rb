df2e8ac295251751c50cb4b6efdf5758002e8111
Daniel Libanori <daniellibanori@gmail.com>
2015-07-10 16:26:37 -0300

    Skip verify filter

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -1,4 +1,6 @@
 class RepliesController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def create
     @appointment = Appointment.find_by(uuid: params[:idMT])
     @appointment.parse(params[:msg])
